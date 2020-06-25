66d244c9482ad870a67753355b82644c920706c5
Tabish idrees <tabishrajput18@gmail.com>
2020-06-25 02:55:12 -0700

    calculaor

---
+++ app.js
@@ -0,0 +1,14 @@
+function picdig(num) {
+    var result = document.getElementById("result")
+    result.value += num
+}
+
+function clearResult() {
+    var result = document.getElementById("result")
+    result.value = ""
+}
+
+function result() {
+    var result = document.getElementById("result");
+    result.value = eval(result.value);
+}
